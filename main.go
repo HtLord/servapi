d166ef737c49b49ce31ec4129acaf57752765689
HtLord <s75757775@hotmail.com>
2019-05-14 14:25:41 +0800

    Add some stdout for debugin why H10 happended.

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"github.com/HtLord/servapi/servapi"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main()  {
+	http.HandleFunc("/create/keeper", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "<h1>My API will work on Heroku. But not sure about db connection.</h1>")
+	})
 	http.HandleFunc("/create/keeper", servapi.CreateKeeper)
 	fmt.Println("Start serving")
+	fmt.Println(os.Getenv("MONGOSECRET"))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
